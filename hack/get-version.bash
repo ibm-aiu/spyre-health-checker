9024d8667d59f7bc0328cd249ae3a4e6225bb4d1
Laurentiu Bradin <laurentiu.bradin@ibm.com>
2026-01-21 13:27:10 +0200

    ci: synchronizes make file with other repos
    Synchronized the changes to Makefile and ci scripts with other repos.

--- hack/get-version.bash
+++ hack/get-version.bash
@@ -1,17 +1,9 @@
 #!/bin/bash
-# Copyright 2024.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# +-------------------------------------------------------------------+
+# | Copyright IBM Corp. 2025 All Rights Reserved                      |
+# | PID 5698-SPR                                                      |
+# +-------------------------------------------------------------------+
+
 set -e
 function usage() {
 	echo "Usage:   get-version.sh current-version"
@@ -26,6 +18,7 @@ function branch_name_check() {
 		${branch_name} =~ ^v[0-9](\.[0-9]+)+-rc\.[0-9]+$ ]]; then
 		echo ${current_version}
 	elif [[ ${branch_name} =~ ^v[0-9]+\.[0-9]+$ ||
+		${branch_name} =~ ^update_to_v[0-9]+(\.[0-9]+)+$ ||
 		${branch_name} == "main" ]]; then
 		echo ${current_version}-dev
 	else
